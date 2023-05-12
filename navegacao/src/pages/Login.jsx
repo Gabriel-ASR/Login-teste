e7b7d89f6a2ea0bce862533d05ad1dbcec71f2ce
unknown <gabriel.a.rocha@iesb.edu.br>
2023-05-11 21:46:07 -0300

    implementação do sistema de entrada realizado com sucesso

--- navegacao/src/pages/Login.jsx
+++ navegacao/src/pages/Login.jsx
@@ -1,17 +1,44 @@
 import { useNavigate } from "react-router-dom";
+import { useRef } from 'react'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 export default function Login(props) {
     const navigate = useNavigate();
+    const refEmail = useRef()
+    const refSenha = useRef()
+    const hookForm = useForm()
+    const { register, handleSubmit } = hookForm
 
-    function handleClick(event) {
-        props.onLogin(event);
-        navigate("/");
-    }
+    const users = [{user: "gabriel04asr@gmail.com", password: "user123"}]
+
+    const handleFormSubmit = (data) => {
+        if (data.email ==  users.map(usur => usur.user) && data.senha == users.map(usur => usur.password)) {
+            props.onLogin(data);
+            navigate("/")
+        }
+        else {
+            console.log("E-mail ou senha incorretos!")
+            console.log(data.email, users.map(usur => usur.user), data.senha, users.map(usur => usur.password))
+        }
+}
 
     return (
-        <>
-            <h1>Login</h1>
-            <button onClick={handleClick}>Entrar</button>
-        </>
+        <div  style={{height: "100vh"}} className="d-flex align-items-center justify-content-center">
+            <div className="p-5 rounded d-flex flex-column w-25 border justify-content-center align-items-center gap-3">
+                <h1 className="text-center">Login</h1>
+                <form onSubmit={handleSubmit(handleFormSubmit)} action="" className=" w-100 d-flex flex-column gap-3 align-items-center justify-content-center">
+                <div className="w-100">
+                    <label htmlFor="email">E-mail</label>
+                    <input {...register("email")} className="form-control w-100" type="email" id="email"/>
+                </div>
+                <div className="w-100">
+                    <label htmlFor="senha">Senha</label>
+                    <input {...register("senha")} className="form-control w-100" type="password" id="senha"/>
+                </div>
+                <button className="btn btn-primary w-100 mt-4">Entrar</button>
+                </form>
+            </div>
+        </div>
     )
 }
